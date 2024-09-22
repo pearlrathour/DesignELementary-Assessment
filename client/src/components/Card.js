c443e146fcb1717dbe166a9efcdff7e0c0579226
Pearl <pearl.rathour33@gmail.com>
2024-09-22 18:58:22 +0530

    Card added

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -2,22 +2,20 @@ import React from 'react';
 
 const ReminderCard = ({ reminder }) => {
     return (
-        <div className="card p-2">
-            {/* <div className="bg-red-400">
-                <div className="toggle-button">
-                    <input type="checkbox" defaultChecked={reminder.isEnabled} />
-                    <span className={reminder.isEnabled ? 'enabled' : 'disabled'} />
+        <div className="card p-4">
+            <div className="bg-slate-300 text-gray-700 text-base rounded-xl">
+                <div className="bg-slate-400 flex flex-row py-1 px-2 rounded-t-xl">
+                    <div className="toggle-button">
+                        <input type="checkbox" defaultChecked={reminder.isEnabled} />
+                        <span className={reminder.isEnabled ? 'enabled' : 'disabled'} />
+                    </div>
+                    <div className='text-gray-800'>Enable/ Disable</div>
                 </div>
-            </div> */}
-            <div className="bg-red-500">
-                <h5 className="card-title">{reminder.title}</h5>
-                <p className="card-text">{reminder.description}</p>
-                <p className="card-text">
-                    <strong>Reminder Date:</strong> {reminder.reminderDate}
-                </p>
-                <p className="card-text">
-                    <strong>Contact No:</strong> {reminder.contactNo}
-                </p>
+                <div className='px-3'>Date: {reminder.date}</div>
+                <div className='px-3'>Email: {reminder.email}</div>
+                <div className='px-3'>Contact No: {reminder.contactno}</div>
+                <div className='px-3'>SMS: {reminder.sms}</div>
+                <div className='px-3 pb-3'>Recur after: {reminder.repeat}</div>
             </div>
         </div>
     );
